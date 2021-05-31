2a39d70aeb597d12f02673e3d733c52529010dba
Al Sayeed <alxayeed@gmail.com>
2021-05-31 17:29:24 +0600

    read pdf text using pdfplumber

--- pdf_reader/pdf_reader.py
+++ pdf_reader/pdf_reader.py
@@ -1,6 +1,7 @@
 import textract
 import urllib.request
 import os
+import pdfplumber
 
 
 def text_tract(file_path):
@@ -15,8 +16,22 @@ def text_tract(file_path):
     return text
 
 
+def use_pdfplumber(file_path):
+    current_directory = os.path.abspath(os.curdir)
+    pdf = current_directory + '/temp.pdf'
+    urllib.request.urlretrieve(file_path, pdf)
+
+    pdf = pdfplumber.open(pdf)
+    page = pdf.pages[0]
+    text = page.extract_text()
+    pdf.close()
+
+    os.remove(pdf)
+    return text
+
+
 if __name__ == '__main__':
     file_path = 'https://www.bfarm.de/SharedDocs/Kundeninfos/EN/01/2021/21994-20_kundeninfo_en.pdf;jsessionid=D2C8EF2B0DE147C315FE08B2985EE04C.1_cid506?__blob=publicationFile&v=2'
 
-    text = text_tract(file_path)
+    text = (file_path)
     print(text)
