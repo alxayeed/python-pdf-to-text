83ba4032cd835c01065e2e243f474f9bf5856f66
Al Sayeed <alxayeed@gmail.com>
2021-05-31 16:32:59 +0600

    read text from pdf url

--- pdf_reader/pdf_reader.py
+++ pdf_reader/pdf_reader.py
@@ -1,13 +1,22 @@
 import textract
 import urllib.request
+import os
 
 
 def text_tract(file_path):
-    text = textract.process(file_path)
+    current_directory = os.path.abspath(os.curdir)
+    pdf = current_directory + '/temp.pdf'
+
+    urllib.request.urlretrieve(file_path, pdf)
+
+    text = textract.process(pdf, encoding='utf-8')
+
+    os.remove(pdf)
     return text
 
 
 if __name__ == '__main__':
-    file_path = 'file/git.pdf'
+    file_path = 'https://www.bfarm.de/SharedDocs/Kundeninfos/EN/01/2021/21994-20_kundeninfo_en.pdf;jsessionid=D2C8EF2B0DE147C315FE08B2985EE04C.1_cid506?__blob=publicationFile&v=2'
+
     text = text_tract(file_path)
     print(text)
