0171482060ed9c03b0badf4501144b5871eb285a
alxayeed <alxayeed@gmail.com>
2021-06-30 12:25:41 +0600

    function splitted into two

--- pdf_reader/pypdf.py
+++ pdf_reader/pypdf.py
@@ -2,11 +2,17 @@ import PyPDF2
 from input_output import links
 
 
-def extract_text_from_pdf(pdf_file):
+def get_pdf_reader_obj(pdf_file):
     # creating a pdf file object
     pdf_file_obj = open(pdf_file, 'rb')
     # creating a PdfFileReader object
     pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+
+    return pdf_reader, pdf_file_obj
+
+
+def extract_text_from_pdf(pdf_file):
+    pdf_reader, pdf_file_obj = get_pdf_reader_obj(pdf_file)
     # prints number of pages
     print(pdf_reader.numPages)
     # extract text of a specific page
